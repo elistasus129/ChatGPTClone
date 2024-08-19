e1bb25ae227a3a11eeeb1bd2c95caca2ce7f73d3
Mert Ramazanoglu <61631884+elistasus129@users.noreply.github.com>
2024-08-19 21:45:35 +0300

    katmanların oluşturulması

--- ChatGPTClone.WebApi/Controllers/WeatherForecastController.cs
+++ ChatGPTClone.WebApi/Controllers/WeatherForecastController.cs
@@ -1,33 +1,22 @@
+using ChatGPTClone.Domain.Entities;
+using ChatGPTClone.Domain.Enums;
+using ChatGPTClone.Domain.ValueObjects;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ChatGPTClone.WebApi.Controllers
+namespace ChatGPTClone.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+       
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-    }
+        
 }
